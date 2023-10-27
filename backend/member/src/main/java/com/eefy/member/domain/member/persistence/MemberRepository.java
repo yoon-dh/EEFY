9b0ed044dc81d58fedda796424b5c441441eec6c
권민정 <22alswjd@naver.com>
2023-10-27 09:14:11 +0900

    fix: entity pk integer로 변경

--- backend/member/src/main/java/com/eefy/member/domain/member/persistence/MemberRepository.java
+++ backend/member/src/main/java/com/eefy/member/domain/member/persistence/MemberRepository.java
@@ -3,5 +3,5 @@ package com.eefy.member.domain.member.persistence;
 import com.eefy.member.domain.member.persistence.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Integer> {
 }
