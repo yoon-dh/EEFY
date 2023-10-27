0dc22a5e2e6c5a16868daf80899f3111cc0649a8
김준석 <pepaa15@naver.com>
2023-10-27 09:37:15 +0900

    rebase issue

--- frontend/EEFY/eefy/src/app/class/layout.tsx
+++ frontend/EEFY/eefy/src/app/class/layout.tsx
@@ -29,33 +29,3 @@ export default function ClassLayout({ children }: { children: React.ReactNode })
     </div>
   );
 }
-
-// export default function ClassLayout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <div>
-//       {/* Container */}
-//       <div className='flex flex-col w-screen h-screen'>
-//         {/* head banner */}
-//         <div className='flex-auto h-1/4'>
-//           <TopNav />
-//         </div>
-
-//         {/* middle content */}
-//         <div className='flex flex-auto w-full h-full '>
-//           <div className='flex-auto w-32 '>
-//             <SideNav />
-//           </div>
-//           <div className='flex flex-auto w-full justify-start items-center'>{children}</div>
-//         </div>
-
-//         {/* Footer */}
-//         <div className='flex-auto h-20 '>
-//           <Footer />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-// export default function ClassLayout({ children }: { children: React.ReactNode }) {
-//   return <div className='w-full h-full'>{children}</div>;
-// }
