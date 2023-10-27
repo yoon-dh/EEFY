6ec8262fbbe29b284b23ca9ed8602de06a99e7db
권민정 <22alswjd@naver.com>
2023-10-27 11:16:19 +0900

    style: member로 도메인명 통일
    - UserRole에서 MemberRole로 변경

--- backend/member/src/main/java/com/eefy/member/domain/member/persistence/entity/enums/MemberRole.java
+++ backend/member/src/main/java/com/eefy/member/domain/member/persistence/entity/enums/MemberRole.java
@@ -3,14 +3,14 @@ package com.eefy.member.domain.member.persistence.entity.enums;
 import lombok.Getter;
 
 @Getter
-public enum UserRole {
+public enum MemberRole {
 
     TEACHER("교사"),
     STUDENT("학생");
 
     private final String value;
 
-    UserRole(String value) {
+    MemberRole(String value) {
         this.value = value;
     }
 }
