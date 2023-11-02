4908b8ade048ef9b85f34431e018975969b13615
권민정 <22alswjd@naver.com>
2023-11-02 10:20:24 +0900

    feat: cors 설정

---
+++ backend/gateway/src/main/java/com/eefy/gateway/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.eefy.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("*"));
+        corsConfig.setMaxAge(8000L);
+        corsConfig.setAllowedMethods(List.of("GET, POST, PUT, DELETE, OPTIONS"));
+        corsConfig.setAllowedHeaders(List.of("Authorization", "Authorization-Refresh"));
+        corsConfig.setExposedHeaders(List.of("Authorization", "Authorization-Refresh"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
