6a7b58308c0213cc8160e7c7e81cdd081d6e35d8
qkdk <akb033134@gmail.com>
2023-11-03 15:20:14 +0900

    feat: swagger 설정

---
+++ backend/homework/src/main/java/com/eefy/homework/global/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.eefy.homework.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Homework API")
+                .description("developed by qkdk")
+                .version("1.0.0"));
+    }
+
+}
