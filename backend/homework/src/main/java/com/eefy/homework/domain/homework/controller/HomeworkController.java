e9cb4ca75800a32fbe3c81836d7c5b0f453131ca
qkdk <akb033134@gmail.com>
2023-11-03 22:50:28 +0900

    fix: 과재 생성 컨트롤러 수정

--- backend/homework/src/main/java/com/eefy/homework/domain/homework/controller/HomeworkController.java
+++ backend/homework/src/main/java/com/eefy/homework/domain/homework/controller/HomeworkController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -41,11 +42,8 @@ public class HomeworkController {
 
         log.info("homework/make/question api 호출 : {}", makeHomeworkQuestionRequest);
 
-        homeworkService.makeQuestion(makeHomeworkQuestionRequest, memberId);
-
         return new ResponseEntity<>(
-            null,
-            HttpStatus.OK
-        );
+            homeworkService.makeQuestion(makeHomeworkQuestionRequest, memberId),
+            HttpStatus.OK);
     }
 }
