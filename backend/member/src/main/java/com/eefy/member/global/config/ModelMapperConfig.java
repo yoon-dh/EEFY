f8f5da93cff6b061d08e72763099e000c19665e3
권민정 <22alswjd@naver.com>
2023-11-04 21:57:58 +0900

    feat: modelMapper 설정

---
+++ backend/member/src/main/java/com/eefy/member/global/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package com.eefy.member.global.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
