c64f6419aebd2450e1f551c70af183f2c8219c58
KodaHye <kodhdh09@gmail.com>
2023-11-05 20:17:18 +0900

    feat: fast api eureka 설정 완료

--- backend/ai/domain/stt/router/stt_router.py
+++ backend/ai/domain/stt/router/stt_router.py
@@ -9,42 +9,7 @@ router = APIRouter(
     prefix="/api/stt"
 )
 
+
 @router.post("")
 async def speech_to_text(file: UploadFile = File(...)):
-    # stt_service.get_speech_to_text(file)
-
-    speech_dir = "domain/stt/speech"
-
-    # # 디렉토리가 없으면 생성
-    if not os.path.exists(speech_dir):
-        os.makedirs(speech_dir)
-
-    # 업로드된 파일의 내용을 읽어서 지정된 경로에 저장
-    file_path = os.path.join(speech_dir, file.filename)
-    with open(file_path, "wb") as audio_file:
-        shutil.copyfileobj(file.file, audio_file)
-
-    stt_service.get_speech_to_text(file_path)
-    # path = await stt_service.get_speech_to_text(file.file)
-    # s3 = s3_connection()
-    #
-    # try:
-    #     s3.upload_file("{로컬에서 올릴 파일이름}","{버킷 이름}","{버킷에 저장될 파일 이름}")
-    # except Exception as e:
-    #     print(e)
-
-    # tmp_directory = "/tmp"
-    # if not os.path.exists(tmp_directory):
-    #     os.makedirs(tmp_directory)
-    #
-    # temp_file_path = os.path.join(tmp_directory, file.filename)
-    #
-    # with open(temp_file_path, "wb") as temp_file:
-    #     shutil.copyfileobj(await file.read(), temp_file)
-    #
-    # transcript = stt_service.get_speech_to_text(temp_file_path)
-    #
-    # os.remove(temp_file_path)
-
-    # return transcript
-
+    return stt_service.get_speech_to_text(file)
---
+++ backend/ai/domain/stt/schemas/stt_result_response.py
@@ -0,0 +1,5 @@
+class SttResultResponse:
+    content: str
+
+    def __init__(self, content: str):
+        self.content = content
