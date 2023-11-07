c6c0145d0333629a79dadd913639768bf36a1beb
권민정 <22alswjd@naver.com>
2023-11-07 17:43:57 +0900

    feat: aop 요청 로깅 추가

--- backend/member/src/main/java/com/eefy/member/MemberApplication.java
+++ backend/member/src/main/java/com/eefy/member/MemberApplication.java
@@ -3,13 +3,15 @@ package com.eefy.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.TimeZone;
 
-@SpringBootApplication
+@EnableAspectJAutoProxy
 @EnableJpaAuditing
 @EnableEurekaClient
+@SpringBootApplication
 public class MemberApplication {
 
     public void setTimeZone() {
---
+++ backend/member/src/main/java/com/eefy/member/global/util/ReadableRequestWrapper.java
@@ -0,0 +1,63 @@
+package com.eefy.member.global.util;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class ReadableRequestWrapper extends HttpServletRequestWrapper {
+    private final Charset encoding;
+    private byte[] rawData;
+
+    public ReadableRequestWrapper(HttpServletRequest request) throws IOException {
+        super(request);
+        String characterEncoding = request.getCharacterEncoding();
+        if (StringUtils.isBlank(characterEncoding)) {
+            characterEncoding = StandardCharsets.UTF_8.name();
+        }
+        this.encoding = Charset.forName(characterEncoding);
+        InputStream inputStream = request.getInputStream();
+        this.rawData = IOUtils.toByteArray(inputStream);
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.rawData);
+        return new ServletInputStream() {
+            @Override
+            public boolean isFinished() {
+                return false;
+            }
+
+            @Override
+            public boolean isReady() {
+                return false;
+            }
+
+            @Override
+            public void setReadListener(ReadListener readListener) {
+            }
+
+            public int read() throws IOException {
+                return byteArrayInputStream.read();
+            }
+        };
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(this.getInputStream(), this.encoding));
+    }
+
+    @Override
+    public ServletRequest getRequest() {
+        return super.getRequest();
+    }
+}
