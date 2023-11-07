bdb6d05ab7870822372921b398ffc53661968f0e
권민정 <22alswjd@naver.com>
2023-11-07 11:32:18 +0900

    fix: JwtTokenResponse 정보 수정

--- backend/member/src/main/java/com/eefy/member/domain/member/dto/response/JwtTokenResponse.java
+++ backend/member/src/main/java/com/eefy/member/domain/member/dto/response/JwtTokenResponse.java
@@ -1,10 +1,15 @@
 package com.eefy.member.domain.member.dto.response;
 
+import com.eefy.member.domain.member.persistence.entity.enums.MemberRole;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class JwtTokenResponse {
-    int userId;
+    private int memberId;
+    private String email;
+    private String name;
+    private String nickname;
+    private MemberRole role;
 }
