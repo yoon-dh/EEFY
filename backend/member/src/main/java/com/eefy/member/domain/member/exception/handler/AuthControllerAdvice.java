809ed69ee41e1f4fac60f3fdf603a279e16a227f
권민정 <22alswjd@naver.com>
2023-11-07 17:45:13 +0900

    feat: 이메일 인증 예외 처리 S09P31B306-210

--- backend/member/src/main/java/com/eefy/member/domain/member/exception/handler/AuthControllerAdvice.java
+++ backend/member/src/main/java/com/eefy/member/domain/member/exception/handler/AuthControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.eefy.member.domain.member.exception.handler;
 
+import com.eefy.member.domain.member.exception.EmailCodeException;
 import com.eefy.member.domain.member.exception.JwtCustomException;
 import com.eefy.member.global.exception.ExceptionResponseDto;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,18 @@ public class AuthControllerAdvice {
 
     @ExceptionHandler(JwtCustomException.class)
     private ResponseEntity<ExceptionResponseDto> jwtException(JwtCustomException e) {
+        log.error(e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(ExceptionResponseDto.builder()
+                        .status(HttpStatus.UNAUTHORIZED)
+                        .code(e.getErrorEnum().getCode())
+                        .message(e.getErrorEnum().getMessage())
+                        .build());
+    }
+
+    @ExceptionHandler(EmailCodeException.class)
+    private ResponseEntity<ExceptionResponseDto> jwtException(EmailCodeException e) {
         log.error(e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
