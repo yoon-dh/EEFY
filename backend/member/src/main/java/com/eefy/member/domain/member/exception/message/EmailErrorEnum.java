a3aad6c0b204c1fd809810a9bebcdc5257f8b6ae
권민정 <22alswjd@naver.com>
2023-11-07 20:12:09 +0900

    fix: 이메일 에러 메세지 수정

--- backend/member/src/main/java/com/eefy/member/domain/member/exception/message/EmailErrorEnum.java
+++ backend/member/src/main/java/com/eefy/member/domain/member/exception/message/EmailErrorEnum.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum EmailErrorEnum {
     NOT_EXIST_EMAIL_CODE(1300, "이메일 인증번호 확인 오류: 이메일 인증 코드가 없습니다."),
     INVALID_EMAIL_CODE_REQUEST(1301, "이메일 인증번호 확인 오류: 이메일 인증 코드가 일치하지 않습니다."),
-    NOT_EXIST_EMAIL_CONFIRM(1302, "이메일 인증번호 확인 오류: "),
+    NOT_EXIST_EMAIL_CONFIRM(1302, "이메일 인증 내역 오류: 인증 내역이 만료되었거나 존재하지 않습니다."),
     EXIST_ACTIVE_EMAIL_CODE(1303, "발급받은 인증 코드 존재: 시간 만료 후 이메일 인증 재요청이 가능합니다."),
     FAILED_EMAIL_CODE_SENDING(1304, "인증 코드 발송 오류: 이메일 인증 코드 발송에 실패했습니다.");
 
