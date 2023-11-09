275a2814a815a6d76110a57d53fc309ce6afcf15
권민정 <22alswjd@naver.com>
2023-11-10 00:15:02 +0900

    fix: openfeign response 수정

--- backend/member/src/main/java/com/eefy/member/domain/studyclass/service/StudyClassService.java
+++ backend/member/src/main/java/com/eefy/member/domain/studyclass/service/StudyClassService.java
@@ -3,8 +3,7 @@ package com.eefy.member.domain.studyclass.service;
 import com.eefy.member.domain.studyclass.dto.response.SearchStudentResponse;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface StudyClassService {
-    Optional<List<SearchStudentResponse>> searchStudentList(int teacherId, int classId);
+    List<SearchStudentResponse> searchStudentList(int teacherId, int classId);
 }
--- backend/member/src/main/java/com/eefy/member/domain/studyclass/service/StudyClassServiceImpl.java
+++ backend/member/src/main/java/com/eefy/member/domain/studyclass/service/StudyClassServiceImpl.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +13,7 @@ public class StudyClassServiceImpl implements StudyClassService {
     private final StudyClassClient studyClassClient;
 
     @Override
-    public Optional<List<SearchStudentResponse>> searchStudentList(int teacherId, int classId) {
+    public List<SearchStudentResponse> searchStudentList(int teacherId, int classId) {
         return studyClassClient.searchStudentList(teacherId, classId);
     }
 }
