f0244abcb8f2ebb35b3bc9de52bf0c7d766d071b
yoondonghun <acttoze1@gmail.com>
2023-11-09 20:45:03 +0900

    fix : fcm api key open

--- frontend/EEFY/eefy/src/app/page.tsx
+++ frontend/EEFY/eefy/src/app/page.tsx
@@ -22,19 +22,19 @@ export default function Home() {
 
     // 이곳에도 아까 위에서 앱 등록할때 받은 'firebaseConfig' 값을 넣어주세요.
     const firebaseApp = initializeApp({
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+      apiKey: 'AIzaSyC7pFu4H7svcrh0RJ_UvKxqrUGZEjJLGXY',
+      authDomain: 'eefy-f2294.firebaseapp.com',
+      projectId: 'eefy-f2294',
+      storageBucket: 'eefy-f2294.appspot.com',
+      messagingSenderId: '433063675765',
+      appId: '1:433063675765:web:751ff0f18e47b9892d353c',
+      measurementId: 'G-V0R74XV218',
     });
 
     const messaging = getMessaging(firebaseApp);
 
     // 이곳 vapidKey 값으로 아까 토큰에서 사용한다고 했던 인증서 키 값을 넣어주세요.
-    getToken(messaging, { vapidKey: process.env.VAPID_KEY })
+    getToken(messaging, { vapidKey: 'BFlWXe5B1irtrj-sT_GtQHYJJ3a4zv562RUM-s8EK-AvD3zIA_ezFEFBRvT_Oa3U1k9HfN3Vh0DjV-MmMvlx8xg' })
       .then(currentToken => {
         if (currentToken) {
           // 정상적으로 토큰이 발급되면 콘솔에 출력합니다.
