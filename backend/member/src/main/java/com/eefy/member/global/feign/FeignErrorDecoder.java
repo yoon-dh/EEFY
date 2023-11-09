23c74ec64f49109eab484162f10b202b2950f3e0
권민정 <22alswjd@naver.com>
2023-11-09 21:45:48 +0900

    feat: openfeign 외부 서비스에 오류가 생겼을 때 예외 처리

---
+++ backend/member/src/main/java/com/eefy/member/global/feign/FeignErrorDecoder.java
@@ -0,0 +1,24 @@
+package com.eefy.member.global.feign;
+
+import com.eefy.member.global.exception.CustomException;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.springframework.http.HttpStatus;
+
+public class FeignErrorDecoder implements ErrorDecoder {
+
+    private final ErrorDecoder defaultErrorDecoder = new Default();
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        if (response.status() >= 400 && response.status() <= 599) {
+            throw CustomException
+                    .builder()
+                    .status(HttpStatus.NOT_FOUND)
+                    .code(1500)
+                    .message("외부 서비스에서 오류 발생")
+                    .build();
+        }
+        return defaultErrorDecoder.decode(methodKey, response);
+    }
+}
