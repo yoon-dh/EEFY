275ddc5084ade004237e849400eee44e913f53f8
권민정 <22alswjd@naver.com>
2023-11-10 22:14:02 +0900

    refactor: passwordEncoder 위치 변경

--- backend/member/src/main/java/com/eefy/member/MemberApplication.java
+++ backend/member/src/main/java/com/eefy/member/MemberApplication.java
@@ -3,8 +3,11 @@ package com.eefy.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableAspectJAutoProxy
 @EnableJpaAuditing
@@ -16,4 +19,8 @@ public class MemberApplication {
         SpringApplication.run(MemberApplication.class, args);
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
