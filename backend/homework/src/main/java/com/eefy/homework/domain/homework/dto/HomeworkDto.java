7bc2ffde87be60ee55ab3b217c46b6fed32aaace
qkdk <akb033134@gmail.com>
2023-11-11 23:34:06 +0900

    fix: 과제 표출 오류 수정

--- backend/homework/src/main/java/com/eefy/homework/domain/homework/dto/HomeworkDto.java
+++ backend/homework/src/main/java/com/eefy/homework/domain/homework/dto/HomeworkDto.java
@@ -3,9 +3,11 @@ package com.eefy.homework.domain.homework.dto;
 import com.eefy.homework.domain.homework.persistence.entity.enums.HomeworkType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class HomeworkDto {
 
     private Integer id;
