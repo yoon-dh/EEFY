78e8bcfdc0f051177ce278cfd9ce958b43c87de9
qkdk <akb033134@gmail.com>
2023-11-12 18:23:59 +0900

    feat: s3파일 읽기 구현

--- backend/ai/domain/announce/router/announce_router.py
+++ backend/ai/domain/announce/router/announce_router.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter, File
 from domain.announce.service.announce_service import evaluate_announce
+from pydantic import BaseModel
+
+class VoiceFilePath(BaseModel):
+    voice_file_path: str
+
 
 router = APIRouter(
     prefix="/api/ai/announce"
 )
 
-@router.get("/evaluate/{voice_file_path}")
-def get_document(voice_file_path):
-    print("announce" + voice_file_path)
-    return evaluate_announce(voice_file_path)
+@router.post("/evaluate/")
+def get_document(voice_file_path: VoiceFilePath):
+    print("announce" + voice_file_path.voice_file_path)
+    return evaluate_announce(voice_file_path.voice_file_path)
--- backend/ai/domain/announce/service/announce_service.py
+++ backend/ai/domain/announce/service/announce_service.py
@@ -3,17 +3,18 @@ import os
 import urllib3
 import json
 import base64
+import urllib.request
 
 openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/Pronunciation"  # 영어
 
 def evaluate_announce(voice_file):
+    response = urllib.request.urlopen(voice_file)
+    data = response.read()
     accessKey = os.getenv("ETRI_API_KEY")
     languageCode = "english"
     # script = "PRONUNCIATION_SCRIPT"
 
-    file = open(voice_file, "rb")
-    audioContents = base64.b64encode(file.read()).decode("utf8")
-    file.close()
+    audioContents = base64.b64encode(data).decode("utf8")
 
     requestJson = {
         "argument": {
