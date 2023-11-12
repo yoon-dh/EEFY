d96fa2051506947159e0e4a237a27bbd775ecbd7
qkdk <akb033134@gmail.com>
2023-11-12 16:35:22 +0900

    feat: 로그 request, response 로그 추가

---
+++ backend/homework/src/main/java/com/eefy/homework/global/config/FilterConfig.java
@@ -0,0 +1,19 @@
+package com.eefy.homework.global.config;
+
+import com.eefy.homework.global.LoggingFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<LoggingFilter> loggingFilter() {
+        FilterRegistrationBean<LoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LoggingFilter());
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+
+}
