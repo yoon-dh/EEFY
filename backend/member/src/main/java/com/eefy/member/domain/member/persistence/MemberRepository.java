0594df86508dc233d75ff9a045fb560a7a667a1d
권민정 <22alswjd@naver.com>
2023-11-14 17:36:51 +0900

    style: 미사용 임포트문 제거

--- backend/member/src/main/java/com/eefy/member/domain/member/persistence/MemberRepository.java
+++ backend/member/src/main/java/com/eefy/member/domain/member/persistence/MemberRepository.java
@@ -1,10 +1,8 @@
 package com.eefy.member.domain.member.persistence;
 
 import com.eefy.member.domain.member.persistence.entity.Member;
-import com.eefy.member.domain.member.persistence.entity.enums.MemberRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
