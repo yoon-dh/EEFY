d0491b629a58a99f99dd7b73c4630873c84f664c
김민균 <rlaalsrbs15@naver.com>
2023-11-17 03:47:56 +0900

    feat: 리모컨 수정

--- frontend/EEFY/eefy/src/components/Main/Library/Homework/Create/Reading/ProblemBox.style.ts
+++ frontend/EEFY/eefy/src/components/Main/Library/Homework/Create/Reading/ProblemBox.style.ts
@@ -7,51 +7,57 @@ font-size: 18px;
 border-radius: 20px;
 `
 export const Wrappe = styled.div`
+overflow: auto;
+&::-webkit-scrollbar {
+    display: none;
+  }
 `
 export const Title = styled.div`
-width: 50%;
+width: 70%;
 position: relative;
 margin: 0px auto 0px auto;
 display: flex;
-top: 30%;
+/* top: 30%; */
 @media(max-width:1340px){
-  top: 30%;
+  /* top: 30%; */
 }
 `
 export const TitleInput = styled.textarea`
-border: 2px solid #D6BCFF;
 border-radius: 8px;
 width: 100%;
 outline: none;
 padding: 10px;
-margin: 0px 30px 0px 0px;
 flex: 7;
 font-size: 14px;
-min-height: 10px;
+resize: none;
+height: 60%;
+word-spacing: 5px;
+background-color: rgb(255, 255, 255, 1);
 `
 export const ContentBox = styled.div`
-width: 50%;
+width: 70%;
 height: 100%;
-margin: 20px auto 20px auto;
+margin: 0px auto 0px auto;
 `
 export const Content = styled.textarea`
-border: 2px solid #D6BCFF;
 border-radius: 8px;
+word-spacing: 5px;
 width: 100%;
-height: 80%;
+height: 100%;
 outline: none;
+resize: none;
 white-space: pre-line;
 word-wrap: break-word;
-padding: 10px;
+padding: 20px;
 font-size: 14px;
 &::-webkit-scrollbar {
     display: none;
   }
 `
 export const NumberBox = styled.div`
-width: 50%;
-height: 80%;
-margin: 10px auto 0px auto;
+width: 70%;
+height: 100%;
+margin: 0px auto 0px auto;
 `
 export const NumberBtn = styled.div``
 export const NumberContent = styled.div``
@@ -59,8 +65,6 @@ export const BtnBox = styled.div`
 width: 100%;
 height: 60%;
 display: flex;
-@media(max-width:1340px){
-}
 `
 export const BeforeBtn = styled.img`
 width: 20px;
@@ -81,13 +85,15 @@ top: 5%;
 margin: 0px 3% 0px auto;
 `
 export const AnswerBox = styled.div`
-border: 1px solid black;
 width: 100px;
 height: 100%;
 flex: 2;
+outline: none;
 `
 export const AnswerInput = styled.input`
 width: 100%;
 padding: 10px;
 font-size: 14px;
+outline: none;
+border-radius: 8px;
 `
