8d0f2395c354caf3ff62ed5402b49c5595644860
KodaHye <kodhdh09@gmail.com>
2023-11-16 17:26:44 +0900

    env: zipkin 설정 추가

--- backend/ai/main.py
+++ backend/ai/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
 from py_eureka_client import eureka_client
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
 from domain.announce.router import announce_router
 from domain.ocr.router import ocr_router
 from domain.stt.router import stt_router
+from starlette_zipkin import ZipkinMiddleware, ZipkinConfig
+
+routes = Route("/", JSONResponse({"status": "OK"}))
 
 app = FastAPI()
+
+config = ZipkinConfig(
+    host="13.209.81.177",
+    port=9411,
+    service_name="ai-service",
+    sample_rate=1.0
+)
+
+app.add_middleware(ZipkinMiddleware, config=config)
 app.include_router(ocr_router.router)
 app.include_router(stt_router.router)
 app.include_router(announce_router.router)
